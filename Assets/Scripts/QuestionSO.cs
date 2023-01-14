f5d199571e20d80871616a527aeee159b1970cb2
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-14 12:29:33 +0800

    Add project files.

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2, 6)]
+    [SerializeField] private string question = "Enter new question text here";
+
+}

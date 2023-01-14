42080d014af0f0f8ad7a33f1bd20e0d974b9fcbc
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-14 19:21:04 +0800

    Add variable to keep track of fill fraction for timer image

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,13 +5,20 @@ public class Timer : MonoBehaviour
     [SerializeField] private float timeToCompleteQuestion = 30f;
     [SerializeField] private float timeToShowCorrectAnswer = 10f;
 
+    public bool loadNextQuestion;
     public bool isAnsweringQuestion = false;
+    public float fillFraction;
 
     private float timerValue;
 
     void Update()
     {
         UpdateTimer();
+    }
+
+    public void CancelTimer()
+    {
+        timerValue = 0;
     }
 
     private void UpdateTimer()
@@ -21,14 +28,24 @@ public class Timer : MonoBehaviour
         // Case guard
         switch (timerValue)
         {
-            case <= 0 when !isAnsweringQuestion:
-                timerValue = timeToCompleteQuestion;
-                isAnsweringQuestion = true;
+            case > 0 when isAnsweringQuestion:
+                fillFraction = timerValue / timeToCompleteQuestion;
                 break;
             case <= 0 when isAnsweringQuestion:
                 timerValue = timeToShowCorrectAnswer;
                 isAnsweringQuestion = false;
                 break;
+
+            case > 0 when !isAnsweringQuestion:
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+                break;
+            case <= 0 when !isAnsweringQuestion:
+                timerValue = timeToCompleteQuestion;
+                isAnsweringQuestion = true;
+                loadNextQuestion = true;
+                break;
+
+
         }
 
 

cfb5bb963f29e2cd0a7ab68ab38960dea1a695eb
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-14 16:52:02 +0800

    Display answer text to buttons

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -5,10 +5,18 @@ public class Quiz : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
+    [SerializeField] private GameObject[] answerButtons;
 
     void Start()
     {
         questionText.text = question.GetQuestion();
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+
     }
 
 }

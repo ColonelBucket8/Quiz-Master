bc38d5f23f004b1c80593cf8ffdaa5825651709b
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-14 12:51:56 +0800

    Add getter method

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -6,4 +6,9 @@ public class QuestionSO : ScriptableObject
     [TextArea(2, 6)]
     [SerializeField] private string question = "Enter new question text here";
 
+    public string GetQuestion()
+    {
+
+        return question;
+    }
 }
